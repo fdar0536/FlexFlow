3ebe674a46ac856b3e34996ec08ac8b98ad5ab92
fdar0536 <fdar0536@users.noreply.github.com>
2025-12-05 05:44:56 +0800

    feat: copy the runtime dll only if file modified

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,26 @@
+/*
+ * Flex Flow
+ * Copyright (c) 2025-present fdar0536
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 use std::env;
 use std::path::PathBuf;
 
@@ -46,15 +69,33 @@ fn main()
     {
         for entry in std::fs::read_dir(&build_dir).unwrap()
         {
-            let path = entry.unwrap().path();
-            if let Some(ext) = path.extension()
+            let src = entry.unwrap().path();
+            if let Some(ext) = src.extension()
             {
                 let ext = ext.to_string_lossy();
                 if matches!(ext.as_ref(), "dll" | "dylib" | "so")
                 {
-                    let file_name = path.file_name().unwrap();
-                    let dest = bundled_dir.join(file_name);
-                    std::fs::copy(&path, &dest).unwrap();
+                    let file_name = src.file_name().unwrap();
+                    let dst = bundled_dir.join(file_name);
+
+                    // copy the file only if modified
+                    if dst.exists()
+                    {
+                        let src_bak = src.clone();
+                        let src_mod = std::fs::metadata(src_bak)
+                        .unwrap().modified().unwrap();
+                        let dst_bak = dst.clone();
+                        let dst_mod = std::fs::metadata(dst_bak)
+                        .unwrap().modified().unwrap();
+                        
+                        if src_mod > dst_mod
+                        {
+                            std::fs::copy(&src, &dst).unwrap();
+                            println!("cargo:warning=lib copy skipped: {} → /", file_name.to_string_lossy());
+                        }
+                    }
+
+                    std::fs::copy(&src, &dst).unwrap();
                     println!("cargo:warning=native lib copied: {} → /", file_name.to_string_lossy());
                 }
             }
